efd417e72f5bbda8c0514db92b254483e303b91b
kaushik1904 <kaushikprajapati1970@gmail.com>
2021-11-19 13:00:05 +0530

    again button updated

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
 
 const displayMessage = message => {
   document.querySelector('.message').innerHTML = message;
@@ -14,10 +14,18 @@ document.querySelector('.check').addEventListener('click', () => {
     displayMessage('Currect Number');
     document.querySelector('.number').innerHTML = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
-    document.querySelector('.number').style.width = '30rem';
   } else if (guess > secretNumber) {
     displayMessage('Too High');
   } else {
     displayMessage('Too Low');
   }
+});
+
+// again button
+document.querySelector('.again').addEventListener('click', () => {
+  displayMessage('Start Guessing...');
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').innerHTML = '?';
+  document.querySelector('.guessNumber').value = '';
 });
